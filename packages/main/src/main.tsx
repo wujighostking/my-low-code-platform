1339d5af12f95a6a179cd9e75ba02a6e5679e90d
gk <promise_ghostking@163.com>
2026-03-09 12:01:52 +0800

    chore: 移除 @unocss/reset 依赖及相关配置

--- packages/main/src/main.tsx
+++ packages/main/src/main.tsx
@@ -4,7 +4,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import 'virtual:uno.css'
-import '@unocss/reset/tailwind.css'
 import 'antd/dist/reset.css'
 
 createRoot(document.getElementById('root')!).render(
