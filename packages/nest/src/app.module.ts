ab319e98a00a586bc73ddf97dce47baf79dc6d45
gk <promise_ghostking@163.com>
2026-03-16 11:32:38 +0800

    refactor(nest): 使用 NestJS CLI 重新初始化项目，修复依赖安装和启动问题

--- packages/nest/src/app.module.ts
+++ packages/nest/src/app.module.ts
@@ -1,28 +1,9 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule, ConfigService } from '@nestjs/config'
-import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { UserModule } from './user/user.module'
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        database: config.get('DB_DATABASE'),
-        username: config.get('DB_USERNAME'),
-        password: config.get('DB_PASSWORD'),
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
-    }),
-    UserModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
