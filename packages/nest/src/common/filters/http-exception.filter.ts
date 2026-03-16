8e420a46d22e8e51c0d478276d3f1919ea71a900
gk <promise_ghostking@163.com>
2026-03-16 14:20:33 +0800

    feat(nest): 添加全局响应拦截与异常过滤器，统一响应格式

---
+++ packages/nest/src/common/filters/http-exception.filter.ts
@@ -0,0 +1,32 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common'
+import { Response } from 'express'
+
+@Catch()
+export class AllExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp()
+    const response = ctx.getResponse<Response>()
+
+    const status
+      = exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR
+
+    const message
+      = exception instanceof HttpException
+        ? exception.message
+        : 'Internal Server Error'
+
+    response.status(status).json({
+      status,
+      data: null,
+      message,
+    })
+  }
+}
